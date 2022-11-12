81de4eddd5dd66803500ed224d16ec19bfab1c95
Sam <89331666+senawka@users.noreply.github.com>
2022-11-12 06:18:12 -0500

    Update fuckyou.py
    debugged

--- fuckyou.py
+++ fuckyou.py
@@ -10,14 +10,14 @@ TOKEN = ''
 role_name = "CatZ Lover"
 
 
-#Triggers
-with open('crankthat/woo.txt', 'r') as f:
+#Filter
+with open('crankthat/souljaboy.txt', 'r') as f:
     global filter
     words = f.read()
     triggerwords = words.split()
 
 #Responses
-with open ('crankthat/souljaboy.txt', 'r') as f:
+with open ('crankthat/woo.txt', 'r') as f:
     global response
     resp = f.read()
     response_list = resp.split(":")
@@ -33,7 +33,7 @@ intent.messages = True
 intent.presences = True
 
 client = discord.Client(intents=intent)
-bot = commands.Bot(command_prefix="*", intents=intent)
+bot = commands.Bot(command_prefix="$", intents=intent)
 
 #activates bot
 @client.event
@@ -57,27 +57,28 @@ async def on_message(message):
             time.sleep(1)
             await recipient.send(random.choice(response_list) + message.author.mention)
         else:
-            await recipient.send(recipient + ' left ')
-        if message.content.startswith('*addfilter'):
-            word = message.content.split('*addfilter', 1)[1]
-            with open('crankthat/woo.txt', 'r') as f:
-                global filter
-                words = f.read()
-                if word.lower() not in words:
-                    with open('crankthat/woo.txt', 'a') as f:
-                        f.write(word + "\n")
-                else:
-                    await message.channel.send('That word is already filtered dipshit.')
-        if message.content.startswith('*addresponse'):
-            phrase = message.content.split('*addresponse', 1)[1]
-            with open ('crankthat/souljaboy.txt', 'r') as f:
-                global filter
-                phrases = f.read()
-                if phrase not in phrases:
-                    with open('crankthat/souljaboy.txt', 'a') as f:
-                        f.write(phrase +":")
-                else:
-                    await message.channel.send('That phrase is already added dipshit.')
+            await recipient.send(recipient + ' left')
+    if message.content.startswith('$addfilter'):
+        word = message.content.split('$addfilter',1)[1]
+        with open('crankthat/souljaboy.txt', 'r') as f:
+            global filter
+            words = f.read()
+            if word.lower() not in words:
+                with open('crankthat/souljaboy.txt', 'a') as f:
+                    f.write(word + "\n")
+            else:
+                await message.channel.send('Added filter.')
+    if message.content.startswith('$addresponse'):
+        phrase = message.content.split('$addresponse',1)[1]
+        with open('crankthat/woo.txt', 'r') as f:
+            global filter
+            phrases = f.read()
+            if phrase not in phrases:
+                with open('crankthat/woo.txt', 'a') as f:
+                    f.write(phrase +":")
+            else:
+                await message.channel.send('Added phrase.')
+
 
 #went completely bald writing this
 
